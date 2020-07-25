7215392730c3ff95cdd53ed4f7b4f9d946a56af7
Ujjwal Singhal <ujjwals@gmail.com>
2020-07-25 12:51:41 +0530

    Updates

---
+++ precache-manifest.ebb1e0204e09977eacac67c911a2888b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a1ac0f8701e402dda5f61c068c6b29",
+    "url": "/throne-of-iris/index.html"
+  },
+  {
+    "revision": "49e5efe401cf58690263",
+    "url": "/throne-of-iris/static/css/2.0da342d8.chunk.css"
+  },
+  {
+    "revision": "a59339171f688ae131fe",
+    "url": "/throne-of-iris/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "49e5efe401cf58690263",
+    "url": "/throne-of-iris/static/js/2.34fe6300.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/throne-of-iris/static/js/2.34fe6300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59339171f688ae131fe",
+    "url": "/throne-of-iris/static/js/main.c43fbd56.chunk.js"
+  },
+  {
+    "revision": "6efe2c91c376b6fc31e2",
+    "url": "/throne-of-iris/static/js/runtime-main.6f4a4119.js"
+  },
+  {
+    "revision": "5d00b5df5787198affd38d966b9224b1",
+    "url": "/throne-of-iris/static/media/bg.5d00b5df.svg"
+  }
+]);
